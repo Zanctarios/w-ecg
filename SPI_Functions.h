616dee05f7df4fc4b5cadd165087bdc891cc7101
Zanctarios <assadalan@gmail.com>
2017-03-27 21:53:14 +0200

    Add files via upload

---
+++ SPI_Functions.h
@@ -0,0 +1,148 @@
+/*
+ * Functions.h
+ *
+ *  Created on: Mar 15, 2017
+ *      Author: Lappy
+ */
+
+#ifndef SPI_FUNCTIONS_H_
+#define SPI_FUNCTIONS_H_
+
+/*First bit 0 is write operation.
+ * Next 7 bits are the register address.
+ * Last 8 bits are the data to be written on the register.
+*/
+
+
+void writeRegister(uint8_t address, uint8_t value, SPI_Handle spiHandle)
+{
+	SPI_Transaction spiTransaction;
+	uint8_t transmit[] = {address & 0x7F, value};    //Value is 2A if AA is input. 42 in Decimal.
+	//uint8_t transmit[] = address;
+	spiTransaction.count = 2;
+	spiTransaction.txBuf = transmit;
+	spiTransaction.rxBuf = NULL;
+	PIN_setOutputValue(csPinHandle, CSPin, 0);
+	SPI_transfer(spiHandle, &spiTransaction);
+	PIN_setOutputValue(csPinHandle, CSPin, 1);
+}
+
+/*First bit 1 is read operation.
+ * Next 7 bits are the register address.
+ * Last 8 bits are the data to be written on the register.
+*/
+
+void readRegister(uint8_t address, SPI_Handle spiHandle)
+{
+	SPI_Transaction spiTransaction;
+	uint8_t receiveBuf[] = {};
+	uint8_t completeAddress[10] = {NULL};
+	completeAddress[0] = address;
+	spiTransaction.count = countSize;
+	spiTransaction.txBuf = completeAddress;
+	spiTransaction.rxBuf = receiveBuf;
+	PIN_setOutputValue(csPinHandle, CSPin, 0);
+	SPI_transfer(spiHandle, &spiTransaction);
+	PIN_setOutputValue(csPinHandle, CSPin, 1);
+}
+
+void readRegCirc(uint8_t address, SPI_Handle spiHandle)
+{
+	SPI_Transaction spiTransaction;
+	uint8_t receiveBuf[] = {};
+	uint8_t completeAddress[7] = {NULL};
+	completeAddress[0] = address;
+	spiTransaction.count = countSize;
+	spiTransaction.txBuf = completeAddress;
+	spiTransaction.rxBuf = receiveBuf;
+	PIN_setOutputValue(csPinHandle, CSPin, 0);
+	SPI_transfer(spiHandle, &spiTransaction);
+	PIN_setOutputValue(csPinHandle, CSPin, 1);
+}
+
+void initSequence(SPI_Handle spiHandle)
+{
+	writeRegister(0x00,0x00,spiHandle);
+	writeRegister(0x01,0x11,spiHandle);
+	writeRegister(0x02,0x19,spiHandle);
+	writeRegister(0x0A,0x07,spiHandle);
+	writeRegister(0x0C,0x04,spiHandle);
+	writeRegister(0x12,0x04,spiHandle);		//Lol
+	writeRegister(0x14,0x24,spiHandle);
+	writeRegister(0x21,0x02,spiHandle);
+	writeRegister(0x22,0x02,spiHandle);
+	writeRegister(0x23,0x02,spiHandle);
+	writeRegister(0x26,0x04,spiHandle);
+	writeRegister(0x27,0x08,spiHandle);
+	writeRegister(0x2F,0x31,spiHandle);
+	writeRegister(0x00,0x01,spiHandle);
+
+	System_printf("Initialization registers written.\n");
+}
+
+void initSequenceCircuit(SPI_Handle spiHandle)
+{
+	writeRegister(0x00,0x00,spiHandle);
+	writeRegister(0x01,0x11,spiHandle);
+	writeRegister(0x0A,0x03,spiHandle);
+	writeRegister(0x0C,0x03,spiHandle);
+	writeRegister(0x12,0x04,spiHandle);
+	writeRegister(0x14,0x39,spiHandle);
+	writeRegister(0x21,0x02,spiHandle);
+	writeRegister(0x22,0x02,spiHandle);
+	writeRegister(0x27,0x08,spiHandle);
+	writeRegister(0x2F,0x11,spiHandle);
+	writeRegister(0x00,0x01,spiHandle);
+
+	System_printf("Initialization registers written.\n");
+}
+
+void factoryReset(SPI_Handle spiHandle)
+{
+	writeRegister(0x00,0x02,spiHandle);
+	writeRegister(0x01,0x00,spiHandle);
+	writeRegister(0x02,0x00,spiHandle);
+	writeRegister(0x03,0x00,spiHandle);
+	writeRegister(0x04,0x00,spiHandle);
+	writeRegister(0x05,0x00,spiHandle);
+	writeRegister(0x06,0x00,spiHandle);
+	writeRegister(0x07,0x00,spiHandle);
+	writeRegister(0x08,0x00,spiHandle);
+	writeRegister(0x09,0x00,spiHandle);
+	writeRegister(0x0A,0x00,spiHandle);
+	writeRegister(0x0B,0x00,spiHandle);
+	writeRegister(0x0C,0x00,spiHandle);
+	writeRegister(0x0D,0x00,spiHandle);
+	writeRegister(0x0E,0x00,spiHandle);
+	writeRegister(0x0F,0x00,spiHandle);
+	writeRegister(0x10,0x00,spiHandle);
+	writeRegister(0x11,0x00,spiHandle);
+	writeRegister(0x12,0x00,spiHandle);
+	writeRegister(0x13,0x00,spiHandle);
+	writeRegister(0x14,0x00,spiHandle);
+	writeRegister(0x15,0x00,spiHandle);
+	writeRegister(0x16,0x00,spiHandle);
+	writeRegister(0x17,0x01,spiHandle);
+	writeRegister(0x1E,0x00,spiHandle);
+	writeRegister(0x1F,0x03,spiHandle);
+	writeRegister(0x21,0x08,spiHandle);
+	writeRegister(0x22,0x80,spiHandle);
+	writeRegister(0x23,0x80,spiHandle);
+	writeRegister(0x24,0x80,spiHandle);
+	writeRegister(0x25,0x00,spiHandle);
+	writeRegister(0x26,0x00,spiHandle);
+	writeRegister(0x27,0x00,spiHandle);
+	writeRegister(0x28,0x40,spiHandle);
+	writeRegister(0x29,0x00,spiHandle);
+	writeRegister(0x2A,0x00,spiHandle);
+	writeRegister(0x2B,0x00,spiHandle);
+	writeRegister(0x2C,0x00,spiHandle);
+	writeRegister(0x2D,0x09,spiHandle);
+	writeRegister(0x2E,0x33,spiHandle);
+	writeRegister(0x2F,0x00,spiHandle);
+	writeRegister(0x40,0x01,spiHandle);
+
+	System_printf("Initialization registers written.\n");
+}
+
+#endif /* SPI_FUNCTIONS_H_ */
---
+++ SPI_Program.c
@@ -0,0 +1,215 @@
+/* XDCtools Header files */
+#include <xdc/std.h>
+#include <xdc/runtime/System.h>
+#include <xdc/runtime/Error.h>
+
+/* BIOS Header files */
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Clock.h>
+#include <ti/sysbios/knl/Task.h>
+#include <xdc/cfg/global.h>
+
+//#include "SPI_Functions.h"
+#define TASKSTACKSIZE   512
+
+/* TI-RTOS Header files */
+#include <ti/drivers/PIN.h>
+#include <ti/drivers/SPI.h>
+#include <ti/drivers/UART.h>
+
+// #include <ti/drivers/Watchdog.h>
+#include <ti/drivers/spi/SPICC26XXDMA.h>
+#include <ti/drivers/dma/UDMACC26XX.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Board Header files */
+#include "Board.h"
+
+#define CSPin Board_DIO21
+uint8_t buffer8BitData[8];
+
+static PIN_Handle csPinHandle;
+static PIN_State csPinState;
+
+Task_Struct spi0TaskStruct;
+Char spi0TaskStack[TASKSTACKSIZE];
+
+PIN_Config csPinTable[] = {
+		CSPin | PIN_GPIO_OUTPUT_EN | PIN_GPIO_HIGH  | PIN_PUSHPULL | PIN_DRVSTR_MAX,
+	    PIN_TERMINATE
+};
+const char initBuff[] = "Normal mode initialized.\n\r";
+const char cirBuff[] = "Circuit mode initialized.\n\r";
+const char factBuff[] = "Factory reset done.\n\r";
+
+const String init = "init";
+const String inic = "inic";
+const String fact = "fact";
+
+int countSize = 2;
+
+#include "SPI_Functions.h"
+
+int hexToDec(char *c)
+{
+	int n= 0, i=0;
+	for(i=0; c[i]!='\0' && c[i]!=0 ; i++){
+	            if(c[i]>='0' && c[i]<='9')
+	                n=n*16+(c[i]-'0');
+	            else if(c[i]=='A' || c[i]=='a')
+	                n=n*16+(10);
+	            else if(c[i]=='B' || c[i]=='b')
+	                n=n*16+(11);
+	            else if(c[i]=='C' || c[i]=='c')
+	                n=n*16+(12);
+	            else if(c[i]=='D' || c[i]=='d')
+	                n=n*16+(13);
+	            else if(c[i]=='E' || c[i]=='e')
+	                n=n*16+(14);
+	            else if(c[i]=='F' || c[i]=='f')
+	                n=n*16+(15);
+	            else{
+	                return -1;
+	            }
+	    }
+	return n;
+}
+
+int codeCommand(char *buffer)
+{
+	if(strcmp(buffer,"init"))
+	{
+		return 1;
+	}
+	if(strcmp(buffer,"inic"))
+	{
+		return 2;
+	}
+	if(strcmp(buffer,"fact"))
+	{
+		return 3;
+	}
+
+	return 4;
+}
+
+
+void spiTaskFxn(UArg arg0, UArg arg1)
+{
+
+		/*Declaration of SPI variables*/
+	    SPI_Handle spiHandle;
+	    SPI_Params SPI_defParams;
+	    UInt spiPeripheralNum = 0;  	/*SPI0*/
+	    UInt uartPeripheralNum = 0;  	/*UART0*/
+
+	    /*Declaration of UART variables*/
+		UART_Handle uartHandle;
+		UART_Params UART_defParams;
+
+		char regBuff[] = "Register sent      \n\r";
+		bool normalMode = true;
+	    /* SPI Parameters initialization */
+	    SPI_Params_init(&SPI_defParams);
+	    SPI_defParams.dataSize = 8;
+	    SPI_defParams.frameFormat = SPI_POL0_PHA0;
+	    SPI_defParams.bitRate = 4000000;
+
+	    /*UART Parameters initialization*/
+	    UART_Params_init(&UART_defParams); //baudRate = 115200; dataLength = 8; stopBits = ONE; No Parity
+
+		spiHandle = SPI_open(spiPeripheralNum, &SPI_defParams);
+		uartHandle = UART_open(uartPeripheralNum,&UART_defParams);
+
+		if(!uartHandle || !spiHandle)
+		{
+			System_printf("Error opening UART or SPI");
+		}
+
+		while(1)
+		{
+
+			char rxBuffer[5] = {NULL};
+			UART_read(uartHandle,rxBuffer,4);
+			UART_write(uartHandle,rxBuffer,sizeof(rxBuffer));
+
+			if(strcmp(rxBuffer,init) == 0)
+			{
+				initSequence(spiHandle);
+				UART_write(uartHandle,initBuff,sizeof(initBuff));
+			}
+			else if(strcmp(rxBuffer,inic) == 0)
+			{
+				initSequenceCircuit(spiHandle);
+				UART_write(uartHandle,cirBuff,sizeof(cirBuff));
+			}
+			else if(strcmp(rxBuffer,fact) == 0)
+			{
+				factoryReset(spiHandle);
+				UART_write(uartHandle,factBuff,sizeof(factBuff));
+			}
+			else
+			{
+				if(normalMode)
+					readRegister(hexToDec(rxBuffer),spiHandle);
+				else
+					readRegCirc(hexToDec(rxBuffer),spiHandle);
+
+				regBuff[14] = rxBuffer[0];
+				regBuff[15] = rxBuffer[1];
+				regBuff[16] = rxBuffer[2];
+				regBuff[17] = rxBuffer[3];
+				UART_write(uartHandle,regBuff,sizeof(regBuff));
+			}
+
+		}
+
+
+	   /* SPI_close(spiHandle);
+	    System_flush();*/
+}
+
+void spiTaskInit()
+{
+		Board_initSPI();
+		Board_initUART();
+
+	    //SPI Task vars
+	    Task_Params spiTaskParams;
+	    Error_Block errBlock;
+
+	    //SPI Task init
+	    Task_Params_init(&spiTaskParams);
+	    Error_init(&errBlock);
+	    spiTaskParams.stackSize = TASKSTACKSIZE;
+	    spiTaskParams.stack = &spi0TaskStack;
+	    spiTaskParams.instance->name = "SPITask";
+
+	    Task_construct(&spi0TaskStruct, (Task_FuncPtr)spiTaskFxn,&spiTaskParams,&errBlock);
+}
+
+/*  ======== main ========
+ */
+
+int main(void)
+{
+    /* Call board init functions */
+    Board_initGeneral();
+
+    csPinHandle = PIN_open(&csPinState, csPinTable);
+	if(!csPinHandle) {
+			System_abort("Error initializing board LED pins\n");
+	 }
+
+    /*Start SPI Task Initialization (Board, Parameters, etc) */
+    spiTaskInit();
+
+    /* Start BIOS */
+    BIOS_start();
+
+    return (0);
+}
+
+
