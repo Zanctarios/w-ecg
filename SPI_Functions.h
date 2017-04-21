6af348b8b1d143ca47ca34eb61af4972216a5640
Zanctarios <assadalan@gmail.com>
2017-04-21 12:53:58 +0200

    Update SPI_Functions.h

--- SPI_Functions.h
+++ SPI_Functions.h
@@ -16,6 +16,7 @@
 
 void writeRegister(uint8_t address, uint8_t value, SPI_Handle spiHandle)
 {
+
 	SPI_Transaction spiTransaction;
 	uint8_t transmit[] = {address & 0x7F, value};    //Value is 2A if AA is input. 42 in Decimal.
 	//uint8_t transmit[] = address;
@@ -32,10 +33,11 @@ void writeRegister(uint8_t address, uint8_t value, SPI_Handle spiHandle)
  * Last 8 bits are the data to be written on the register.
 */
 
-void readRegister(uint8_t address, SPI_Handle spiHandle)
+void readRegister(uint8_t address, SPI_Handle spiHandle, UART_Handle uartHandle)
 {
+	countSize = 2;
 	SPI_Transaction spiTransaction;
-	uint8_t receiveBuf[] = {};
+	uint8_t receiveBuf[10] = {NULL};
 	uint8_t completeAddress[10] = {NULL};
 	completeAddress[0] = address;
 	spiTransaction.count = countSize;
@@ -44,10 +46,12 @@ void readRegister(uint8_t address, SPI_Handle spiHandle)
 	PIN_setOutputValue(csPinHandle, CSPin, 0);
 	SPI_transfer(spiHandle, &spiTransaction);
 	PIN_setOutputValue(csPinHandle, CSPin, 1);
+	UART_write(uartHandle,receiveBuf,sizeof(receiveBuf));
 }
 
-void readRegCirc(uint8_t address, SPI_Handle spiHandle)
+void readRegCirc(uint8_t address, SPI_Handle spiHandle, UART_Handle uartHandle)
 {
+	countSize = 7;
 	SPI_Transaction spiTransaction;
 	uint8_t receiveBuf[] = {};
 	uint8_t completeAddress[7] = {NULL};
@@ -58,6 +62,7 @@ void readRegCirc(uint8_t address, SPI_Handle spiHandle)
 	PIN_setOutputValue(csPinHandle, CSPin, 0);
 	SPI_transfer(spiHandle, &spiTransaction);
 	PIN_setOutputValue(csPinHandle, CSPin, 1);
+	UART_write(uartHandle,receiveBuf,sizeof(receiveBuf));
 }
 
 void initSequence(SPI_Handle spiHandle)
@@ -72,7 +77,7 @@ void initSequence(SPI_Handle spiHandle)
 	writeRegister(0x21,0x02,spiHandle);
 	writeRegister(0x22,0x02,spiHandle);
 	writeRegister(0x23,0x02,spiHandle);
-	writeRegister(0x26,0x04,spiHandle);
+	//writeRegister(0x26,0x04,spiHandle);
 	writeRegister(0x27,0x08,spiHandle);
 	writeRegister(0x2F,0x31,spiHandle);
 	writeRegister(0x00,0x01,spiHandle);
@@ -105,7 +110,7 @@ void factoryReset(SPI_Handle spiHandle)
 	writeRegister(0x03,0x00,spiHandle);
 	writeRegister(0x04,0x00,spiHandle);
 	writeRegister(0x05,0x00,spiHandle);
-	writeRegister(0x06,0x00,spiHandle);
+	writeRegister(0x06,0x08,spiHandle);
 	writeRegister(0x07,0x00,spiHandle);
 	writeRegister(0x08,0x00,spiHandle);
 	writeRegister(0x09,0x00,spiHandle);
